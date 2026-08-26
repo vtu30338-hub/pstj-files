27afb0c57377309ae9105eb1cf56fb69dc84be55
vtu30338-hub <vtu30338@veltech.edu.in>
2026-08-27 01:43:14 +0530

    Create Session 4 Task 7 VTU 30338.java

---
+++ week 4/Week 4/Session 4 Task 7 VTU 30338.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+
+    int n = arr.size();
+
+    int primary = 0;
+    int secondary = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        // Left-to-right diagonal
+        primary += arr.get(i).get(i);
+
+        // Right-to-left diagonal
+        secondary += arr.get(i).get(n - 1 - i);
+    }
+
+    return Math.abs(primary - secondary);
+}
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
