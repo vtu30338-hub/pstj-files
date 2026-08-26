dbd2f785b4269fb34f038603c55b74d8bf807b7f
vtu30338-hub <vtu30338@veltech.edu.in>
2026-08-27 00:29:45 +0530

    Create Session 2 Java Sort VTU 30338.java

---
+++ Week 1/Week 1/Week 1/Session 2 Java Sort VTU 30338.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class Student {
+    private int id;
+    private String fname;
+    private double cgpa;
+
+    public Student(int id, String fname, double cgpa) {
+        this.id = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        List<Student> studentList = new ArrayList<Student>();
+
+        while (testCases > 0) {
+
+            int id = in.nextInt();
+            String fname = in.next();
+            double cgpa = in.nextDouble();
+
+            Student st = new Student(id, fname, cgpa);
+            studentList.add(st);
+
+            testCases--;
+        }
+
+        Collections.sort(studentList, new Comparator<Student>() {
+            @Override
+            public int compare(Student s1, Student s2) {
+
+                if (Double.compare(s2.getCgpa(), s1.getCgpa()) != 0) {
+                    return Double.compare(s2.getCgpa(), s1.getCgpa());
+                }
+
+                if (!s1.getFname().equals(s2.getFname())) {
+                    return s1.getFname().compareTo(s2.getFname());
+                }
+
+                return Integer.compare(s1.getId(), s2.getId());
+            }
+        });
+
+        for (Student st : studentList) {
+            System.out.println(st.getFname());
+        }
+
+        in.close();
+    }
+}
