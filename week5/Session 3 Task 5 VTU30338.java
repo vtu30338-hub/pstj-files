5e9c55cd747672d45d13ed62c9d801dd3a29c238
vtu30338-hub <vtu30338@veltech.edu.in>
2026-09-17 01:35:07 +0530

    Create Session 3 Task 5 VTU30338.java

---
+++ week5/Session 3 Task 5 VTU30338.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int myAtoi(String s) {
+        
+        int i = 0;
+        int n = s.length();
+        int sign = 1;
+        long result = 0;
+
+        // Step 1: Skip leading spaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // Step 2: Check sign
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i) == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+
+        // Step 3: Convert digits
+        while (i < n && Character.isDigit(s.charAt(i))) {
+
+            result = result * 10 + (s.charAt(i) - '0');
+
+            // Step 4: Check overflow
+            if (sign == 1 && result > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+
+            if (sign == -1 && -result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+
+            i++;
+        }
+
+        return (int) (sign * result);
+    }
+}
