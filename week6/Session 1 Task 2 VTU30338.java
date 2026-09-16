025dff62d517881f63898f0e26911ace62576105
vtu30338-hub <vtu30338@veltech.edu.in>
2026-09-17 01:52:55 +0530

    Create Session 1 Task 2 VTU30338.java

---
+++ week6/Session 1 Task 2 VTU30338.java
@@ -0,0 +1,11 @@
+class Solution {
+
+    public boolean repeatedSubstringPattern(String s) {
+
+        String doubled = s + s;
+
+        String modified = doubled.substring(1, doubled.length() - 1);
+
+        return modified.contains(s);
+    }
+}
